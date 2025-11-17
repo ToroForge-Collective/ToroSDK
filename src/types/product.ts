4d84bff0dbc8feb714222fa72d1d925ef016360f
milimeter <razaqolad300@gmail.com>
2025-11-17 12:49:52 +0100

    Add GetProjectInput and ProductInput types to product.ts for product management functionality

---
+++ src/types/product.ts
@@ -0,0 +1,14 @@
+export type GetProjectInput = {
+  admin: string;
+  getbalances?: string;
+};
+
+export type ProductInput = {
+  productId: string;
+  productName: string;
+  description: string;
+  productImage: string;
+  admin: string;
+  adminpwd: string;
+};
+
