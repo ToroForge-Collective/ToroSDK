f109979ad327e41249a268f0873056fbaf44c00f
milimeter <razaqolad300@gmail.com>
2025-12-01 10:15:35 +0100

    Refactor deposit test to include KYC verification and commented out additional functionality for future implementation. This enhances the test coverage for deposit operations while maintaining clarity in the code.

--- examples/deposit.test.ts
+++ examples/deposit.test.ts
@@ -1,7 +1,36 @@
-import { confirmDeposit, depositFunds } from "../src";
+import {
+  confirmDeposit,
+  depositFunds,
+  getFiatTransactionByTxid,
+  getSupportedAssetsExchangeRates,
+  isAddressKYCVerified,
+} from "../src";
 import { Currency } from "../src/types/currency";
 
 async function deposit() {
+  const kycVerified = await isAddressKYCVerified({
+    address: "0x0Ca0b83FD81D726f52C266bB617a14a4Ec80214A",
+  });
+  console.log("✓ KYC verified:", kycVerified);
+  // const exchangeRates = await getSupportedAssetsExchangeRates();
+  // console.log("✓ Exchange rates:", exchangeRates);
+  // const fiatTransactionDetails = await getFiatTransactionByTxid({
+  //   txid: "0101768404",
+  //   admin: "",
+  //   adminpwd: "",
+  // });
+  // console.log("✓ Fiat transaction details:", fiatTransactionDetails);
+  // if (
+  //   fiatTransactionDetails?.data &&
+  //   Array.isArray(fiatTransactionDetails.data) &&
+  //   fiatTransactionDetails.data.length > 0
+  // ) {
+  //   const txHash = fiatTransactionDetails.data[0].TX_ChainTXID;
+  //   console.log("✓ TX hash:", txHash);
+  // } else {
+  //   console.error("No transaction data found or invalid response structure");
+  // }
+
   // const depositDetails = await depositFunds(
   //   {
   //     userAddress: "0x8d043e12a02f2d2cc38ed1ad3f50de4aba064c6c",
@@ -19,11 +48,11 @@ async function deposit() {
   // console.log("✓ Deposit initialized:", depositDetails);
 
   // Verify deposit
-  const isDepositConfirmed = await confirmDeposit({
-    currency: Currency.Naira,
-    transactionId: "0101764699",
-  });
-  console.log("✓ Deposit confirmed:", isDepositConfirmed);
+  // const isDepositConfirmed = await confirmDeposit({
+  //   currency: Currency.Naira,
+  //   transactionId: "0101764699",
+  // });
+  // console.log("✓ Deposit confirmed:", isDepositConfirmed);
 
   // KYC operations
   // const kycParams = {
